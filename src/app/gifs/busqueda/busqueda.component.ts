123ceff2b5c3264b2d9dea432ef7a292fc1ab106
Bnd6 <sinistetraa@gmail.com>
2021-12-16 16:05:15 -0500

    Agregando git app

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,30 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../service/gifs.service';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  constructor(private gifsService: GifsService) { }
+
+
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+
+    if (valor.trim().length === 0) {
+      return;
+    }
+
+    console.log(valor)
+    this.txtBuscar.nativeElement.value = ""
+    this.gifsService.buscarGifs(valor);
+  }
+
+}
